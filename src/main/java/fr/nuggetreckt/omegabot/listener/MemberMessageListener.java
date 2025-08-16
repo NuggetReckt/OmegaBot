ae7a42d3910ca0c76dcf66801899a2c8a4571833
NuggetReckt <corto.morrow@gmail.com>
2025-08-16 17:11:23 +0200

    fix (Counter): Now handling messages editing action that reevaluates next messages

--- src/main/java/fr/nuggetreckt/omegabot/listener/MemberMessageListener.java
+++ src/main/java/fr/nuggetreckt/omegabot/listener/MemberMessageListener.java
@@ -4,18 +4,57 @@ import fr.nuggetreckt.omegabot.OmegaBot;
 import fr.nuggetreckt.omegabot.statistics.MemberStats;
 import fr.nuggetreckt.omegabot.statistics.StatsHandler;
 import fr.nuggetreckt.omegabot.util.ParseUtil;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberMessageListener extends ListenerAdapter {
 
     private final OmegaBot instance;
+    private final List<String> messagesToSkip;
+
+    private final Emoji INVALID_EMOJI = Emoji.fromUnicode("❌");
 
     public MemberMessageListener(OmegaBot instance) {
         this.instance = instance;
+        this.messagesToSkip = new ArrayList<>();
+    }
+
+    @Override
+    public void onMessageUpdate(@NotNull MessageUpdateEvent event) {
+        if (event.getAuthor().isBot()) return;
+        if (!event.getChannel().getId().equals(instance.getConfig().getCountChannel().getId())) return;
+
+        Member author = event.getMember();
+        Message message = event.getMessage();
+
+        assert author != null;
+        if (!handleMessage(author, message)) {
+            handleInvalidMessage(message);
+            return;
+        }
+        MessageHistory history = event.getChannel().getHistoryAfter(message, 100).complete();
+        List<Message> messages = history.getRetrievedHistory();
+
+        handleValidMessage(message);
+        for (Message msg : messages) {
+            Member mb = msg.getMember();
+
+            if (mb == null || msg.getAuthor().isBot()) continue;
+            if (handleMessage(mb, msg)) {
+                handleValidMessage(msg);
+            } else {
+                handleInvalidMessage(msg);
+            }
+        }
     }
 
     @Override
@@ -23,22 +62,36 @@ public class MemberMessageListener extends ListenerAdapter {
         if (event.getAuthor().isBot()) return;
         if (!event.getChannel().getId().equals(instance.getConfig().getCountChannel().getId())) return;
 
-        StatsHandler statsHandler = instance.getStatsHandler();
+        Member author = event.getMember();
         Message message = event.getMessage();
+
+        assert author != null;
+        if (!handleMessage(author, message)) {
+            handleInvalidMessage(message);
+        }
+    }
+
+    private boolean handleMessage(@NotNull Member author, @NotNull Message message) {
+        StatsHandler statsHandler = instance.getStatsHandler();
         String content = ParseUtil.splitMessage(message.getContentRaw());
-        MemberStats memberStats = statsHandler.getMemberStats(event.getAuthor().getId());
+        MemberStats memberStats = statsHandler.getMemberStats(author.getId());
+        Message before = getValidMessageBefore(message);
+        long beforeValue;
         long count;
 
-        if (!ParseUtil.isMessageValid(content)) {
-            handleInvalidMessage(message);
-            return;
+        if (!ParseUtil.isMessageValid(content) || before == null) {
+            return false;
         }
         count = Long.parseLong(content);
+        beforeValue = Long.parseLong(before.getContentRaw());
 
-        if (count != statsHandler.getCurrentNum() + 1) {
-            handleInvalidMessage(message);
-            return;
+        if (count != beforeValue + 1) {
+            return false;
         }
+        if (messagesToSkip.contains(message.getId())) {
+            return true;
+        }
+        messagesToSkip.add(message.getId());
         if (content.endsWith("69")) {
             message.addReaction(Emoji.fromFormatted("\uD83D\uDE0F")).queue();
             memberStats.magicNumberCount++;
@@ -49,9 +102,32 @@ public class MemberMessageListener extends ListenerAdapter {
         }
         memberStats.counted++;
         statsHandler.setCurrentNum(statsHandler.getCurrentNum() + 1);
+        return true;
     }
 
     private void handleInvalidMessage(@NotNull Message message) {
-        message.addReaction(Emoji.fromUnicode("❌")).queue();
+        message.addReaction(INVALID_EMOJI).queue();
+    }
+
+    private void handleValidMessage(@NotNull Message message) {
+        if (!messagesToSkip.contains(message.getId())) {
+            messagesToSkip.add(message.getId());
+        }
+        message.removeReaction(INVALID_EMOJI).queue();
+    }
+
+    private Message getValidMessageBefore(@NotNull Message message) {
+        MessageHistory history = message.getChannel().getHistoryBefore(message, 5).complete();
+        Message before = null;
+
+        for (Message msg : history.getRetrievedHistory()) {
+            if (msg.getAuthor().isBot()) continue;
+
+            if (!msg.getReactions().equals(INVALID_EMOJI)) {
+                before = msg;
+                break;
+            }
+        }
+        return before;
     }
 }
